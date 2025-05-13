102435da18a0bafa9d107995b4167af3d3914151
Vinícius Prado Batista <viniciusprado7606@gmail.com>
2025-05-13 20:15:34 -0300

    NodeApi

---
+++ apps/node-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+// Endpoint que consome a API Python
+app.get('/from-python', async (req, res) => {
+  try {
+    const response = await axios.get('http://python_api:5000/');
+    res.send({ messageFromPython: response.data });
+  } catch (error) {
+    console.error('Erro ao chamar a API Python:', error.message);
+    res.status(500).send('Erro ao chamar a API Python');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Node.js API rodando em http://localhost:${PORT}`);
+});
